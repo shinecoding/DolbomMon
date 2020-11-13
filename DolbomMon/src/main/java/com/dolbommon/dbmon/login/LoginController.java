ec2bb0c94d06485a90dc2285015ed4e7349742ca
dydwnsppq <dydwnsppq@gmail.com>
2020-11-13 17:53:37 +0900

    11
    11

--- DolbomMon/src/main/java/com/dolbommon/dbmon/login/LoginController.java
+++ DolbomMon/src/main/java/com/dolbommon/dbmon/login/LoginController.java
@@ -29,5 +29,31 @@ public class LoginController {
 	
 		return null;	
 	}
+	
+	// 선생님, 부모님 회원가입 선택
+	@RequestMapping("/join")
+	public String join() {
+		return "register/join";
+	}
+	
+	// 학부모 회원가입 시작
+	@RequestMapping("/parentJoinStart")
+	public String joinParentStart() {
+		return "register/parent/regForm";
+	}
+	
+	// 돌봄몬 회원가입 시작
+	@RequestMapping("/dbmJoinStart")
+	public String joinDbmStart() {
+		return "register/dbm/start";
+	}
+	
+	// 우편코드 선택창
+	@RequestMapping("/zipcodeSearch")
+	public String zipcodeSearch() {
+		
+		return "register/zipcodeSearch";
+	}
+	
 
 }
