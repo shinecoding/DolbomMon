0147e83c384db46abad7008a08f9da98175598f8
seulgi4229 <seulgi4229@naver.com>
2020-11-20 17:33:28 +0900

    1120 1733

--- DolbomMon/src/main/java/com/dolbommon/dbmon/board/BoardController.java
+++ DolbomMon/src/main/java/com/dolbommon/dbmon/board/BoardController.java
@@ -7,7 +7,11 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +30,9 @@ public class BoardController {
 		this.sqlSession = sqlSession;
 	}
 	
+	@Autowired
+	DataSourceTransactionManager transactionManager;	
+	
 	//게시판 리스트로 이동ok
 	@RequestMapping("/freeBoard")
 	public ModelAndView freeBoard() {
@@ -41,18 +48,6 @@ public class BoardController {
 		
 		return mav;
 	}
-
-	//총 게시물 수 구하기
-//	public int getTotalRecord() {
-		
-//		FreeBoardDaoImp dao = sqlSession.getMapper(FreeBoardDaoImp.class);
-//		int totalRecord = dao.getTotalRecord();
-//		
-//		ModelAndView mav = new ModelAndView();
-		
-		
-//		return totalRecord;
-//	}
 	
 	//게시판 글쓰기 폼으로 이동ok
 	@RequestMapping("/freeBoardWrite")
@@ -141,6 +136,52 @@ public class BoardController {
 		return mav;
 	}
 	
+	//자유게시판 답글 쓰기 폼으로 이동
+	@RequestMapping("/freeBoardReplyForm")
+	public String replyWrite(int no, Model model) {
+		model.addAttribute("no", no);
+			
+		return "freeBoard/freeBoardReplyForm";
+	}
+	
+	//자유게시판 답글 쓰기
+	@RequestMapping(value="/freeBoardReplyOk", method=RequestMethod.POST)
+	public ModelAndView freeBoardReplyOk(FreeBoardVO vo, HttpSession ses) {
+		vo.setUserid((String)ses.getAttribute("userid"));
+		
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setPropagationBehavior(DefaultTransactionDefinition.PROPAGATION_REQUIRED);
+				
+		TransactionStatus status = transactionManager.getTransaction(def);
+		
+		FreeBoardDaoImp dao = sqlSession.getMapper(FreeBoardDaoImp.class);
+		
+		//원글의 ref, step, lvl 선택하기
+		FreeBoardVO optVo = dao.optionSelect(vo.getNo());
+
+		try {
+			dao.levelUpdate(optVo);
+			
+			//답글 쓰기
+			vo.setRef(optVo.getRef());
+			vo.setStep(optVo.getStep()+1);
+			vo.setLvl(optVo.getLvl()+1);
+			
+			System.out.println("55555");
+			
+			int result = dao.replyBoardInsert(vo);
+			
+			System.out.println("66666");
+			
+			transactionManager.commit(status);
+		}catch(Exception e) {
+			transactionManager.rollback(status);
+		}	
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:freeBoard");
+		return mav;
+	}
+	
 	//공지사항 게시판으로 이동ok
 	@RequestMapping("/noticeBoard")
 	public String noticeBoard() {
@@ -154,6 +195,8 @@ public class BoardController {
 		
 		return "freeBoard/noticeBoardWrite";
 	}
+	
+	
 	
 //	//공지사항 게시글 보기
 //	@RequestMapping(value="/noticeBoardView", method=RequestMethod.POST)
